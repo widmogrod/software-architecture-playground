4a21314881ededecc93273733cfe11f11a6cb709
widmogrod <widmogrod@gmail.com>
2020-09-27 18:53:54 +0200

    Clean up implementation

--- clean-vertical/essence/usecase/confirm_account_activation_spec.go
+++ clean-vertical/essence/usecase/confirm_account_activation_spec.go
@@ -8,17 +8,15 @@ import (
 )
 
 func SpecConfirmAccountActivation(t *testing.T) {
-	t.Run("ConfirmAccountActivation: When register new account, you must activate it and get access token", func(t *testing.T) {
-		email := EmailAddress("user-eamil-2@example.com")
+	email := EmailAddress("user-eamil-2@example.com")
+	ctx := context.Background()
 
-		ctx := context.Background()
+	t.Run("ConfirmAccountActivation: When register new account, you must activate it and get access token", func(t *testing.T) {
 		result := dispatch.Invoke(ctx, RegisterAccountWithEmail{EmailAddress: email})
 		rorwe := result.(ResultOfRegisteringWithEmail)
 		if !rorwe.IsSuccessful() {
 			t.Fatal("fresh registration didn't succeed")
 		}
-
-		t.Log(rorwe.SuccessfulResult)
 
 		res := dispatch.Invoke(ctx, ConfirmAccountActivation{
 			ActivationToken: rorwe.SuccessfulResult.ActivationTokenTestOnly,
