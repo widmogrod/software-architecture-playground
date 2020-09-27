0e1f4879035096809c2da7d53db6e86a3b97415f
widmogrod <widmogrod@gmail.com>
2020-09-27 18:51:29 +0200

    Introduce specification for GenerateSessionToken

---
+++ clean-vertical/essence/usecase/generate_session_token_spec.go
@@ -0,0 +1,25 @@
+package usecase
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/widmogrod/software-architecture-playground/clean-vertical/essence/algebra/dispatch"
+	"testing"
+	"time"
+)
+
+func SpecGenerateSessionToken(t *testing.T) {
+	uuid := time.Now().String()
+	t.Run("GenerateSessionToken: should generate session token", func(t *testing.T) {
+		ctx := context.Background()
+		cmd := GenerateSessionToken{
+			UserUUID: uuid,
+		}
+
+		res := dispatch.Invoke(ctx, cmd)
+		resgst := res.(ResultOfGeneratingSessionToken)
+		assert.NotEmpty(t, resgst.SuccessfulResult.AccessToken, "")
+		assert.NotEmpty(t, resgst.SuccessfulResult.RefreshToken, "")
+		assert.NotEqual(t, resgst.SuccessfulResult.AccessToken, resgst.SuccessfulResult.RefreshToken)
+	})
+}
