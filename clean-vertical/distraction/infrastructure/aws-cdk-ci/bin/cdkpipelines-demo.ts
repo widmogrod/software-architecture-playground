f6963b117def19dc2a4e8f2667af2ecb5d474907
widmogrod <widmogrod@gmail.com>
2020-10-03 18:14:01 +0200

    Introduce Dev Stage

--- clean-vertical/distraction/infrastructure/aws-cdk-ci/bin/cdkpipelines-demo.ts
+++ clean-vertical/distraction/infrastructure/aws-cdk-ci/bin/cdkpipelines-demo.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import {CdkpipelinesDemoStage} from "../lib/cdkpipelines-demo-stage";
 
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {});
+
+new CdkpipelinesDemoStage(app, 'Dev', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
 
 app.synth();
