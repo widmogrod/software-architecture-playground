4a5c7cfe21d32bda314116333d53b5c38d1198cd
widmogrod <widmogrod@gmail.com>
2020-10-03 16:54:16 +0200

    Introduce draft of cdk ci/cd

---
+++ clean-vertical/distraction/infrastructure/aws-cdk-ci/bin/cdkpipelines-demo.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {});
+
+app.synth();
---
+++ clean-vertical/distraction/infrastructure/aws-cdk-ci/lib/cdkpipelines-demo-pipeline-stack.ts
@@ -0,0 +1,44 @@
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkpipelinesDemoPipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        const pipeline = new CdkPipeline(this, 'Pipeline', {
+            // The pipeline name
+            pipelineName: 'MyServicePipeline',
+            cloudAssemblyArtifact,
+
+            // Where the source can be found
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub',
+                output: sourceArtifact,
+                oauthToken: SecretValue.secretsManager('github-token'),
+                owner: 'widmogrod',
+                repo: 'software-architecture-playground',
+            }),
+
+            // How it will be built and synthesized
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+
+                // We need a build step to compile the TypeScript Lambda
+                buildCommand: 'npm run build',
+                subdirectory: 'clean-vertical/distraction/infrastructure/aws-cdk-ci'
+            }),
+        });
+
+        // This is where we add the application stages
+        // ...
+    }
+}
