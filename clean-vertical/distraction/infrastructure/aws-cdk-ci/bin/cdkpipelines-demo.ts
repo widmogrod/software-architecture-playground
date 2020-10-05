d658cda3dc715e8423c32d27ed0cdda57bf3181b
widmogrod <widmogrod@gmail.com>
2020-10-05 20:47:42 +0200

    Remove dev pipeline-demo-stage

--- clean-vertical/distraction/infrastructure/aws-cdk-ci/bin/cdkpipelines-demo.ts
+++ clean-vertical/distraction/infrastructure/aws-cdk-ci/bin/cdkpipelines-demo.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
-import {CdkpipelinesDemoStage} from "../lib/cdkpipelines-demo-stage";
 import {CleanVerticalRestStack} from "../lib/clean-vertical-rest-stack";
 import {CleanVerticalHttpStack} from "../lib/clean-vertical-http-stack";
 
@@ -9,9 +8,6 @@ const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {});
 
-new CdkpipelinesDemoStage(app, 'Dev', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
 new CleanVerticalRestStack(app, 'DevCleanVerticalRestAPI', {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 })
