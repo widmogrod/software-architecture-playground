b2bf321a5cd7b0917227319f767148c98f2cdcfb
widmogrod <widmogrod@gmail.com>
2020-10-21 21:08:13 +0200

    Experiment with AWS AppConfig

--- clean-vertical/distraction/infrastructure/aws-native/functions/appconfig/index.py
+++ clean-vertical/distraction/infrastructure/aws-native/functions/appconfig/index.py
@@ -1,5 +1,4 @@
 import urllib.request
-import json
 
 def handler(event, context):
     url = f'http://localhost:2772/applications/test-app/environments/dev-env/configurations/test-clean-vertical-conf-profile'
