ae8b5443c416fcc8f0799698b8daeacf86cf58f7
widmogrod <widmogrod@gmail.com>
2020-10-24 16:34:42 +0200

    Refactor global Program

--- clean-vertical/essence/usecase/confirm_account_activation.go
+++ clean-vertical/essence/usecase/confirm_account_activation.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	dispatch.Register(HandleConfirmAccountActivation)
+	dispatch.RegisterGlobalHandler(HandleConfirmAccountActivation)
 }
 
 type ConfirmAccountActivation struct {
