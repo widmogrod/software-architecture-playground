4718e25c52b702bdc639956b8ee352d5fed1c496
widmogrod <widmogrod@gmail.com>
2021-01-03 18:38:58 +0100

    Introduce new test cases

--- clean-vertical/essence/usecase/generate_session_token_spec.go
+++ clean-vertical/essence/usecase/generate_session_token_spec.go
@@ -17,8 +17,21 @@ func SpecGenerateSessionToken(t *testing.T) {
 
 		res := dispatch.Invoke(ctx, cmd)
 		resgst := res.(ResultOfGeneratingSessionToken)
-		assert.NotEmpty(t, resgst.SuccessfulResult.AccessToken, "")
-		assert.NotEmpty(t, resgst.SuccessfulResult.RefreshToken, "")
-		assert.NotEqual(t, resgst.SuccessfulResult.AccessToken, resgst.SuccessfulResult.RefreshToken)
+		assert.NotEmpty(t, resgst.SuccessfulResult.AccessToken, "AccessToken is empty")
+		assert.NotEmpty(t, resgst.SuccessfulResult.RefreshToken, "RefreshToken is empty")
+		assert.NotEqual(t, resgst.SuccessfulResult.AccessToken, resgst.SuccessfulResult.RefreshToken, "AccessToken must be different to RefreshToken")
+	})
+
+	t.Run("GenerateSessionToken: consecutive token generation should create unique tokens each time", func(t *testing.T) {
+		ctx := dispatch.Background()
+		cmd := GenerateSessionToken{
+			UserUUID: uuid,
+		}
+
+		token1 := dispatch.Invoke(ctx, cmd).(ResultOfGeneratingSessionToken).SuccessfulResult
+		token2 := dispatch.Invoke(ctx, cmd).(ResultOfGeneratingSessionToken).SuccessfulResult
+
+		assert.NotEqual(t, token1.AccessToken, token2.AccessToken, "newly created AccessToken is identical to previous and MUST NOT be")
+		assert.NotEqual(t, token1.RefreshToken, token2.RefreshToken, "newly created RefreshToken is identical to previous and MUST NOT be")
 	})
 }
