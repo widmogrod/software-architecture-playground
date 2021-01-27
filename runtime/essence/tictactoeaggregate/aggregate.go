07a4a5f46ec07eae1222ca451ec644d94a86b583
widmogrod <widmogrod@gmail.com>
2021-01-27 22:51:47 +0100

    Simplify aggregate for tictactoe

--- runtime/essence/tictactoeaggregate/aggregate.go
+++ runtime/essence/tictactoeaggregate/aggregate.go
@@ -1,32 +1,20 @@
 package tictactoeaggregate
 
 import (
-	"github.com/widmogrod/software-architecture-playground/runtime"
 	"github.com/widmogrod/software-architecture-playground/runtime/essence/algebra/aggregate"
 )
 
 func NewTicTacToeAggregate() *TicTacToeAggregate {
 	store := aggregate.NewEventStore()
-	aggregate := &TicTacToeAggregate{
+	return &TicTacToeAggregate{
 		state:   nil,
 		changes: store,
-		ref: &runtime.AggregateRef{
-			ID:   "",
-			Type: "tictactoe",
-		},
 	}
-
-	return aggregate
 }
 
 type TicTacToeAggregate struct {
 	state   *TicTacToeState
 	changes *aggregate.EventStore
-	ref     *runtime.AggregateRef
-}
-
-func (o *TicTacToeAggregate) Ref() *runtime.AggregateRef {
-	return o.ref
 }
 
 func (o *TicTacToeAggregate) State() interface{} {
@@ -35,10 +23,4 @@ func (o *TicTacToeAggregate) State() interface{} {
 
 func (o *TicTacToeAggregate) Changes() *aggregate.EventStore {
 	return o.changes
-}
-func (o *TicTacToeAggregate) Hydrate(state interface{}, ref *runtime.AggregateRef) error {
-	o.state = state.(*TicTacToeState)
-	o.ref = ref
-
-	return nil
 }
