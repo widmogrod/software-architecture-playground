1acbc9d145fc087b17257a2db5462ace3354fba6
widmogrod <widmogrod@gmail.com>
2022-03-16 19:18:16 +0100

    go generics: Proxy for method receiver pattern :/

--- hypo/gogeneric/functional.go
+++ hypo/gogeneric/functional.go
@@ -19,3 +19,55 @@ func Compose[A, B, C any](f func(A) B, g func(B) C) func(A) C {
 		return g(f(a))
 	}
 }
+
+func Identity[T any](x T) T {
+	return x
+}
+
+type Container[T any] interface {
+	*T
+}
+
+type Functor[T any] interface {
+	Join() T
+}
+
+// err: generic type cannot be alias
+//type MapFunc[T1, T2 any] = func(T1) T2
+
+type Functor2[T1, T2 any] interface {
+	Return(T2) Functor[T2]
+	Map(func(T1) T2) Functor[T2]
+}
+
+type Functor3[T any, T1 Functor[T], T2 any] interface {
+	*T1
+	Map(func(T1) T2) Functor[T2]
+}
+
+//func Map[T1, T2 any](f func(T1) T2, x Functor[T1]) Functor[T2] {
+//	y := Functor2[T1, T2]{x}
+//return y.Return(f(x.Join()))
+//}
+
+type IdentityM[T1 any] struct {
+	v T1
+}
+
+func (i *IdentityM[T1]) Return(t T1) Functor[T1] {
+	return &IdentityM[T1]{v: t}
+}
+
+func (i *IdentityM[T1]) Join() T1 {
+	return i.v
+}
+
+//var _ Functor[int] = IdentityM[int]
+
+type ProxyF[T any, T1 Functor[T], T2 any] struct {
+	v T1
+}
+
+func (p ProxyF[T, T1, T2]) Map(f func(T) T2) Functor[T2] {
+	return &IdentityM[T2]{v: f(p.v.Join())}
+}
--- hypo/gogeneric/functional_test.go
+++ hypo/gogeneric/functional_test.go
@@ -2,6 +2,7 @@ package gogeneric
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/widmogrod/software-architecture-playground/hypo/gogeneric/x/lists"
 	"testing"
 )
 
@@ -16,4 +17,43 @@ func TestCurry(t *testing.T) {
 	com := Compose(add(2), mul(10))
 
 	assert.Equal(t, com(3), (3+2)*10)
+}
+
+//func FunctorLaw[T1, T2, T3 any](t *testing.T, f func(T1) T2, g func(T2) T3, m Functor[T1, T3]) {
+//	// identity: fmap id  ==  id
+//	assert.Equal(t, m.Map(Identity[T1]), m)
+//
+//	// composition: fmap (f . g)  ==  fmap f . fmap g
+//
+//	assert.Equal(t, m.Map(Compose(f, g)), m.Map(f).Map(g))
+//}
+
+func TestMap(t *testing.T) {
+	var l lists.List[int]
+	l.Push(1)
+
+	//f := func(x int) string {
+	//	return strconv.Itoa(x) + "+ 2"
+	//}
+	//g := func(x string) string {
+	//	return x + "4"
+	//}
+	f := func(x int) int {
+		return x + 2
+	}
+	g := func(x int) int {
+		return x + 4
+	}
+
+	Compose(f, g)(1)
+
+	m := &IdentityM[int]{v: 1}
+	p := ProxyF[int, *IdentityM[int], int]{v: m}
+	// identity: fmap id  ==  id
+
+	assert.Equal(t, m, p.Map(Identity[int]))
+
+	// composition: fmap (f . g)  ==  fmap f . fmap g
+
+	//assert.Equal(t, p.Map(Compose(f, g)), p.Map(f).Map(g)))
 }
