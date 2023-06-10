f0e44181c7dda6402cb658541abde331c35b5d9f
widmogrod <widmogrod@gmail.com>
2023-06-10 22:36:18 +0200

    clean up tictactoe-aws cdk

--- eventsourcing/distraction/tictactoe-aws/bin/tictactoe-app.ts
+++ eventsourcing/distraction/tictactoe-aws/bin/tictactoe-app.ts
@@ -1,19 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {ContinuousDeliveryStack} from "../lib/continuous-delivery-stack";
-import {DatabaseStack} from "../lib/database-stack";
-import {WebsocketStack} from "../lib/websocket-stack";
 import {WebsocketSqSStack} from "../lib/websocket-sqs-stack";
 import {StaticWebsiteStack} from "../lib/static-website-stack";
-import {FargateStack} from "../lib/fargate-stack";
-import {EssenceTestStack} from "../lib/essence-test-stack";
 
 const app = new cdk.App();
-// new ContinuousDeliveryStack(app, 'ContinuousDeliveryStack', {});
-// new DatabaseStack(app, 'DevDatabaseStack', {});
-// new WebsocketStack(app, 'WebsocketStack', {});
-// new FargateStack(app, 'FargateStack', {});
 new WebsocketSqSStack(app, 'WebsocketSqSStack', {});
 new StaticWebsiteStack(app, 'StaticWebsiteStack', {});
-new EssenceTestStack(app, 'EssenceTestStack', {});
